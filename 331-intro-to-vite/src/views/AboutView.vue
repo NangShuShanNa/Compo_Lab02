a8f818ef071b088df3a45cefa61ecdf5d3deb519
NangShuShanNa <nangshushanna_nang@cmu.ac.th>
2025-08-18 22:01:13 +0700

    Responsive for about page is done

--- 331-intro-to-vite/src/views/AboutView.vue
+++ 331-intro-to-vite/src/views/AboutView.vue
@@ -3,6 +3,14 @@
 </script>
 <template>
     <div class="about">
-        This is About page
     </div>
 </template>
+<style>
+@media(min-width: 1024px){
+    .about{
+         min-height: 100vh;
+    display: flex;
+    align-items: center;
+    }
+}
+</style>
